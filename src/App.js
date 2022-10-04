fb2cf87561f9cc8c23f296b7132287e4b5c928f2
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-04 15:47:21 +0100

    chore:added the button and onclick function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import data from "./data";
+import data from "./Data";
 import List from "./List";
 function App() {
   const [people, setPeople] = useState(data);
@@ -9,6 +9,7 @@ function App() {
         <h2>Birthday Reminder</h2>
         <h3>0 Birthday today</h3>
         <List />
+        <button onClick={() => {}}>Clear All</button>
       </section>
     </main>
   );
